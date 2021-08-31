76ed030250efaa6044ab43f1c9846af7598a5669
ydk1204 <ejrbdi@naver.com>
2021-08-31 15:57:21 +0900

    comand modify

---
+++ dd.js
@@ -0,0 +1,3 @@
+let count = prompt();
+
+console.log(count);
---
+++ hello.py
@@ -0,0 +1,8 @@
+count = int(input())
+li = []
+
+for i in range(count) :
+    s = int(input())
+    li.append(s) 
+
+print(min(li), max(li))
